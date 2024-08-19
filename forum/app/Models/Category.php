51ae3b9a7a5b72f87c04a2ad1fe5b18b5388978f
FELIPE SALCEDO RAMALHO <0031432412008@lti.fatecso.edu.br>
2024-08-19 18:06:42 -0300

    forum

--- forum/app/Models/Category.php
+++ forum/app/Models/Category.php
@@ -5,16 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tag extends Model
+class Category extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'title'
+        'title',
+        'description'
     ];
-    
+
     public function topics()
     {
-        return $this->belongsToMany(Topic::class, 'topic_tag');
+        return $this->hasMany(Topic::class);
     }
 }
