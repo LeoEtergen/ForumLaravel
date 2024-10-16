276812c2815b8e7e2b6c192a71b6f76d92763991
FELIPE SALCEDO RAMALHO <0031432412008@lti.fatecso.edu.br>
2024-10-16 16:31:58 -0300

    forum

--- forum/app/Models/Topic.php
+++ forum/app/Models/Topic.php
@@ -15,6 +15,11 @@ class Topic extends Post
         'status'
     ];
 
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+    
     public function category()
     {
         return $this->belongsTo(Category::class);
